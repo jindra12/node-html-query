e4b93a4f189eaa2ddef784a440aeed3a99eb04fb
jindra12.underdark <jindra12.underdark@gmail.com>
2024-01-29 00:31:27 +0100

    Add another HTML test

--- test/file.test.ts
+++ test/file.test.ts
@@ -3,16 +3,24 @@ import path from "path";
 import { Query } from "../src/query";
 import { parseLexer, parsedHtmlLexer } from "../src/parser";
 
-const file = fs.readFileSync(path.join(__dirname, "developer.mozilla.org.html"), { encoding: "utf-8" });
+const files = [
+    fs.readFileSync(path.join(__dirname, "developer.mozilla.org.html"), { encoding: "utf-8" }),
+    fs.readFileSync(path.join(__dirname, "jsdom.html"), { encoding: "utf-8" }),
+];
 
 const parseQueue = (input: string) => parseLexer(input, parsedHtmlLexer).queue;
 
 describe("Can parse and query full html file", () => {
-    it("Can transcribe the whole file in lexer and return it back", () => {
-        expect(parseQueue(file).map((item) => item.value).join("")).toEqual(file);
+    files.forEach((file, index) => {
+        it(`Can transcribe the whole file in lexer and return it back - ${index}`, () => {
+            expect(parseQueue(file).map((item) => item.value).join("")).toEqual(file);
+        });
+        it(`Parses whole file without throwing - ${index}`, () => {
+            expect(() => Query(file)).not.toThrow();
+        });
     });
     it("Can parse a complex html file", () => {
-        const $ = Query(file);
+        const $ = Query(files[0]);
         expect($("div").length).toEqual(142);
         expect($("header h1").text()).toEqual(":nth-child()");
         expect($("a")[0].attr("href")).toEqual("#content");
