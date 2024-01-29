a2a830f9b99f40afb95bd5ea9cd25f1912051e7d
jindra12.underdark <jindra12.underdark@gmail.com>
2024-01-29 21:54:21 +0100

    Add more file tests

--- test/file.test.ts
+++ test/file.test.ts
@@ -3,24 +3,26 @@ import path from "path";
 import { Query } from "../src/query";
 import { parseLexer, parsedHtmlLexer } from "../src/parser";
 
-const files = [
-    fs.readFileSync(path.join(__dirname, "developer.mozilla.org.html"), { encoding: "utf-8" }),
-    fs.readFileSync(path.join(__dirname, "jsdom.html"), { encoding: "utf-8" }),
-];
+const files = {
+    "developer.mozilla.org.html": fs.readFileSync(path.join(__dirname, "developer.mozilla.org.html"), { encoding: "utf-8" }),
+    "jsdom.html": fs.readFileSync(path.join(__dirname, "jsdom.html"), { encoding: "utf-8" }),
+    "wikipedia.html": fs.readFileSync(path.join(__dirname, "wikipedia.html"), { encoding: "utf-8" }),
+    "webpack.html": fs.readFileSync(path.join(__dirname, "webpack.html"), { encoding: "utf-8" }),
+};
 
 const parseQueue = (input: string) => parseLexer(input, parsedHtmlLexer).queue;
 
 describe("Can parse and query full html file", () => {
-    files.forEach((file, index) => {
-        it(`Can transcribe the whole file in lexer and return it back - ${index}`, () => {
+    Object.entries(files).forEach(([fileName, file]) => {
+        it(`Can transcribe the whole file in lexer and return it back - ${fileName}`, () => {
             expect(parseQueue(file).map((item) => item.value).join("")).toEqual(file);
         });
-        it(`Parses whole file without throwing - ${index}`, () => {
+        it(`Parses whole file without throwing - ${fileName}`, () => {
             expect(() => Query(file)).not.toThrow();
         });
     });
-    it("Can parse a complex html file", () => {
-        const $ = Query(files[0]);
+    it("Can parse and query a complex html file", () => {
+        const $ = Query(files["developer.mozilla.org.html"]);
         expect($("div").length).toEqual(142);
         expect($("header h1").text()).toEqual(":nth-child()");
         expect($("a")[0].attr("href")).toEqual("#content");
