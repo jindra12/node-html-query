d2ba0aed8a006603921c762884876eb1a720c9af
jindra12.underdark <jindra12.underdark@gmail.com>
2024-02-03 14:08:45 +0100

    Bootstrap more complex speed test

--- test/speed.test.ts
+++ test/speed.test.ts
@@ -7,14 +7,81 @@ import { Query } from "../src";
 
 import v8Profiler from 'v8-profiler-next';
 v8Profiler.setGenerateType(1);
-const ourTitle = "my-speed-test";
-const theirTitle = "their-speed-test";
 
 const file = fs.readFileSync(path.join(__dirname, "developer.mozilla.org.html"), { encoding: "utf-8" });
 const iterations = 5;
 const my$ = Query(file);
 const dom = new jsdom.JSDOM(file);
 const their$ = jQuery(dom.window);
+
+interface Test {
+    name: string;
+    query: (query: typeof my$) => any;
+}
+
+const tests: Test[] = [
+    {
+        name: "find-div",
+        query: $ => $("div"),
+    },
+    {
+        name: "find-toggle",
+        query: $ => $(".toggle"),
+    },
+    {
+        name: "append-div",
+        query: $ => $("div").append("<p>Appended to div!</p>"),
+    },
+    {
+        name: "append-toggle",
+        query: $ => $(".toggle").append("<p>Appended to div!</p>"),
+    },
+];
+
+const profilePath = path.join(__dirname, "..", "profile");
+
+fs.mkdirSync(profilePath);
+
+const runTest = (test: Test) => {
+    it(`Has faster ${test.name} than competitors`, () => {
+        const ourTitle = `my-speed-test-${test.name}`;
+        const theirTitle = `their-speed-test-${test.name}`;
+
+        const testPath = path.join(__dirname, "..", "profile", test.name);
+
+        fs.mkdirSync(testPath)
+
+        v8Profiler.startProfiling(ourTitle, true);
+        const beforeMine = performance.now();
+        for (let i = 0; i < iterations; i++) {
+            test.query(my$);
+        }
+        const afterMine = performance.now();
+        const myProfile = v8Profiler.stopProfiling(ourTitle);
+        myProfile.export((_, result: any) => {
+            fs.writeFileSync(path.join(__dirname, "..", "profile", test.name, `my.cpuprofile`), result);
+            myProfile.delete();
+        });
+    
+        const mine = afterMine - beforeMine;
+        v8Profiler.startProfiling(theirTitle, true);
+        const beforeTheirs = performance.now();
+        for (let i = 0; i < iterations; i++) {
+            test.query((their$ as any));
+        }
+        const afterTheirs = performance.now();
+        const theirProfile = v8Profiler.stopProfiling(theirTitle);
+        theirProfile.export((_, result: any) => {
+            fs.writeFileSync(path.join(__dirname, "..", "profile", test.name, `their.cpuprofile`), result);
+            theirProfile.delete();
+        });
+
+        const theirs = afterTheirs - beforeTheirs;
+
+        console.log(`faster ${test.name} than theirs by ${Math.round(100 * (theirs - mine) / theirs)}%`);
+        expect(mine).toBeLessThan(theirs);
+    });
+};
 
 describe("Node-html should be faster than jQuery with JSDOM", () => {
     it("Has faster initialization time than competition", () => {
@@ -36,60 +103,5 @@ describe("Node-html should be faster than jQuery with JSDOM", () => {
         console.log(`faster init than theirs by ${Math.round(100 * (theirs - mine) / theirs)}%`)
         expect(mine).toBeLessThan(theirs);
     });
-    it("Has faster DOM find", () => {
-        v8Profiler.startProfiling(ourTitle, true);
-        const beforeTheirs = performance.now();
-        for (let i = 0; i < iterations; i++) {
-            (their$ as any)("div");
-        }
-        const afterTheirs = performance.now();
-        const theirProfile = v8Profiler.stopProfiling(ourTitle);
-        theirProfile.export((_, result: any) => {
-            fs.writeFileSync(`${theirTitle}.cpuprofile`, result);
-            theirProfile.delete();
-        });
-
-        const theirs = afterTheirs - beforeTheirs;
-
-        v8Profiler.startProfiling(ourTitle, true);
-        const beforeMine = performance.now();
-        for (let i = 0; i < iterations; i++) {
-            my$("div");
-        }
-        const afterMine = performance.now();
-        const myProfile = v8Profiler.stopProfiling(ourTitle);
-        myProfile.export((_, result: any) => {
-            fs.writeFileSync(`${ourTitle}.cpuprofile`, result);
-            myProfile.delete();
-        });
-    
-        const mine = afterMine - beforeMine;
-
-        console.log(`faster find than theirs by ${Math.round(100 * (theirs - mine) / theirs)}%`);
-        expect(mine).toBeLessThan(theirs);
-
-        expect(my$("div").length).toEqual(142);
-        expect(their$.find("div").length).toEqual(142);
-    });
-    it("Has faster DOM alter", () => {
-        const beforeMine = performance.now();
-        for (let i = 0; i < iterations; i++) {
-            my$("div").append("<p>Appended to div!</p>"); 
-        }
-        const afterMine = performance.now();
-        const mine = afterMine - beforeMine;
-
-        const beforeTheirs = performance.now();
-        for (let i = 0; i < iterations; i++) {
-            (their$ as any)("div").append("<p>Appended to div!</p>"); 
-        }
-        const afterTheirs = performance.now();
-        const theirs = afterTheirs - beforeTheirs;
-
-        console.log(`faster append than theirs by ${Math.round(100 * (theirs - mine) / theirs)}%`);
-        expect(mine).toBeLessThan(theirs);
-
-        expect(my$("div").length).toEqual(142);
-        expect(their$.find("div").length).toEqual(142);
-    });
+    tests.forEach(runTest);
 });
